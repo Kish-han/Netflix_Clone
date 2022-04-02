611b1fea2950276d0345e3a712f9235de76c2a35
Kishan <Kishanprasad09102001@gmail.com>
2022-04-03 00:38:32 +0530

    Movie list added

--- netflix-clone/src/components/Row/Row.js
+++ netflix-clone/src/components/Row/Row.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from '../../axios'
+import React, { useEffect, useState } from 'react'
 import './Row.css'
-function Row() {
-  return (
+    
+    function Row({ title, fetchUrl, isLargeRow = false }) {
+    
+        const [movies, setMovies] = useState([]);
+
+        const base_url = "https://image.tmdb.org/t/p/original/"
+        useEffect(() => {
+            async function fetchData() {
+                const request = await axios.get(fetchUrl);
+                setMovies(request.data.results);
+                return request
+            }
+
+            fetchData();
+        }, [fetchUrl]);
+    
+    console.log(movies)
+
+    return (
       <div className='row'>
-          <h2>This is a TITLE</h2>
+            <h2>{title}</h2>
+            <div className='row__posters'>
+                {movies.map(
+                    (movie) =>
+                        ((isLargeRow && movie.poster_path) ||
+                        (!isLargeRow && movie.backdrop_path)) && (
+                            <img
+                                className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${base_url}${
+                                    isLargeRow ? movie.poster_path : movie.backdrop_path
+                                    }`}
+                                alt={movie.name} />        
+                        )
+                )}
+            </div>
       </div>
   )
 }
