e67cdfce38b61a8fa0efe290f85c6ba7533fd885
Kishan <Kishanprasad09102001@gmail.com>
2022-04-02 19:01:19 +0530

    Add banner done

--- netflix-clone/src/components/HomeScreen/HomeScreen.js
+++ netflix-clone/src/components/HomeScreen/HomeScreen.js
@@ -1,15 +1,27 @@
 import React from 'react'
-// import Banner from './Banner/Banner'
+import requests from '../../Request'
+import Banner from '../Banner/Banner'
 import './HomeScreen.css'
-import Nav from './Navbar/Nav'
+import Nav from '../Navbar/Nav'
+import Row from '../Row/Row'
+
 
 function HomeScreen() {
   return (
       <div className='homescreen'>
           <Nav />
-          {/* <Banner /> */}
-          {/* Banner */}
-
+          <Banner />
+          <Row
+            title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals}
+              isLargeRow
+          />
+          <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+          <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+          <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+          <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+          <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
 
       </div>
   )
