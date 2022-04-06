d789fc684f8feb48d2d6c72dd4fe911fa2c4bc1c
Kishan <Kishanprasad09102001@gmail.com>
2022-04-06 23:09:17 +0530

    Final build

--- netflix-clone/src/components/Navbar/Nav.js
+++ netflix-clone/src/components/Navbar/Nav.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Nav.css'
+import { auth } from "../../firebase"
+
 
 function Nav() {
 
@@ -20,16 +22,28 @@ function Nav() {
         return () => window.removeEventListener('scroll', transitionNavBar);
     }, []);
 
+    // const [toggle, setToggle] = useState(false);
+
+    // const signOutAppear = () => {
+    //     if (tog) {
+            
+    //     }
+    // }
+
     return (
         <div className={`nav ${show && "nav__black"}`}>
           <div className='nav__content'>
                 <img
                     onClick={()=> navigate('/')}
                     className='nav__logo' src='https://www.freepnglogos.com/uploads/netflix-logo-0.png' alt='NetflixLogo' />
-
-                <img 
-                    onClick={() => navigate('/profile')}
-                    className='nav__avatar' src='https://imgs.search.brave.com/RpmEWWVvrBoGYAReKTbSB7waKTNR68q8N7mxbr1MXEo/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5t/UlZXd2xKdXVYLU9k/RnQ1Tmp5R1lRSGFI/YSZwaWQ9QXBp' alt='' />
+                <div>
+                    <img 
+                        className='nav__avatar' src='https://imgs.search.brave.com/RpmEWWVvrBoGYAReKTbSB7waKTNR68q8N7mxbr1MXEo/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5t/UlZXd2xKdXVYLU9k/RnQ1Tmp5R1lRSGFI/YSZwaWQ9QXBp' alt='' />
+                    <div onClick={() => auth.signOut() } className='signout'>
+                        <p>Sign Out</p>
+                    </div>
+                </div>    
+                
           </div>
       </div>
   )
--- netflix-clone/src/components/ProfileScreen/ProfileScreen.js
+++ netflix-clone/src/components/ProfileScreen/ProfileScreen.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectuser } from '../../features/counter/userSlice'
-import Nav from '../Navbar/Nav'
-import './ProfileScreen.css'
-import { auth } from "../../firebase"
+// import React from 'react'
+// import { useSelector } from 'react-redux'
+// import { selectuser } from '../../features/counter/userSlice'
+// import Nav from '../Navbar/Nav'
+// import './ProfileScreen.css'
+// import { auth } from "../../firebase"
 
-function ProfileScreen() {
-  const user = useSelector(selectuser);
+// function ProfileScreen() {
+//   const user = useSelector(selectuser);
 
-  return (
-    <div className='profileScreen'>
-      <Nav />
-      <div className='profile_screen_body'>
-        <h1>Edit profile</h1>
-        <div className='profileScreen_info'>
-          <img src='https://imgs.search.brave.com/RpmEWWVvrBoGYAReKTbSB7waKTNR68q8N7mxbr1MXEo/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5t/UlZXd2xKdXVYLU9k/RnQ1Tmp5R1lRSGFI/YSZwaWQ9QXBp' alt='Avatarimg' />
-          <div className='profileScreen_details'>
-            <h2>{user.email}</h2>
-            <div className='profileScreen_plans'>
-              <h3>Plans</h3>
-              <button onClick={()=> auth.signOut() } className='profileScreen_signout'>Sign Out</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className='profileScreen'>
+//       <Nav />
+//       <div className='profile_screen_body'>
+//         <h1>Edit profile</h1>
+//         <div className='profileScreen_info'>
+//           <img src='https://imgs.search.brave.com/RpmEWWVvrBoGYAReKTbSB7waKTNR68q8N7mxbr1MXEo/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5t/UlZXd2xKdXVYLU9k/RnQ1Tmp5R1lRSGFI/YSZwaWQ9QXBp' alt='Avatarimg' />
+//           <div className='profileScreen_details'>
+//             <h2>{user.email}</h2>
+//             <div className='profileScreen_plans'>
+//               <h3>Plans</h3>
+//               <button onClick={()=> auth.signOut() } className='profileScreen_signout'>Sign Out</button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default ProfileScreen
+// export default ProfileScreen
